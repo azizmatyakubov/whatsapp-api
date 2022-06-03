026c4c2ccd016ead520836e6354df046fa438430
AZIZ <a.matyaqubov1471@gmail.com>
2022-06-03 16:38:16 +0200

    create socket server

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 import express from "express";
 import cors from 'cors'
-
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
-
 app.get('/api/test', (req, res) => {
     res.send({
         message: 'Test seccessful'
     })
 })
+
+
+
+io.on("connection", (socket) => {
+    console.log("New client connected");
+    console.log(socket.id)
+
+    socket.emit("message", {
+        message: "Welcome to the chat app"
+        })
+
+    socket.on("sendMessage", (data) => {
+        console.log(data)
+        io.emit("message", data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("Client disconnected")
+    })
+})
+
+  
 
 
 export default app;
