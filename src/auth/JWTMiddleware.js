8eebd316f1efb439141871c42fa449b87a6968e7
AZIZ <a.matyaqubov1471@gmail.com>
2022-06-03 17:44:19 +0200

    fix jwt generate bug

--- src/auth/JWTMiddleware.js
+++ src/auth/JWTMiddleware.js
@@ -2,11 +2,13 @@ import createError from "http-errors";
 import { verifyAccessToken } from "./tool.js";
 
 export const JWTAuthMiddleware = async (req, res, next) => {
+  console.log(req);
   if (!req.cookies.accessToken) {
     next(createError(401, "Send Access Token"));
   } else {
     try {
       const token = req.cookies.accessToken;
+      console.log(token)
       const payload = await verifyAccessToken(token);
 
       req.user = {
